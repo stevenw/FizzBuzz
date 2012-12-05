794660332147ffbaca72929e85ebecd2f3300e48
Steven <stevenwisniewski@gmail.com>
2012-12-05 16:12:34 -0500

    Echos without newlines

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,2 +1,20 @@
 <?PHP
-	
+	for ($i = 1; $i <= 100; $i++) 
+	{
+		if ($i % 3 === 0 && $i % 5 === 0) 
+		{
+			echo 'FizzBuzz';
+		}
+		else if($i % 3 === 0)
+		{
+			echo 'Fizz';
+		}
+		else if($i % 5 === 0)
+		{
+			echo 'Buzz';
+		}
+		else
+		{
+			echo $i;
+		}
+	}
